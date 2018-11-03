05aef161ad8877f656c5f12046cdd22fe22389c2
Akshay Sadarangani <akshay.sadarangani@gmail.com>
2018-11-03 19:05:49 -0400

    Setup MEAN structure

---
+++ public/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Akshay on 11/03/2018.
+ */
+(function () {
+    angular
+        .module("Datis", ["ngRoute", "textAngular"]);
+})();
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+app.use(session({
+    secret: 'this is the secret',    // process.env.SESSION_SECRET
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+require ("./test/app.js")(app);
+
+//require("./passport/services/user.service.server");
+var port = process.env.PORT || 3000;
+
+app.listen(port);
