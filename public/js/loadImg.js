4360fa240c170f9da30df0de5df746ab7fb52dc9
Akshay Sadarangani <akshay.sadarangani@gmail.com>
2018-11-04 11:44:51 -0500

    Login/Registration templates

---
+++ public/js/loadImg.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Akshay on 11/03/2018.
+ */
+
+$(window).on('load', (function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");
+}));
---
+++ server/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Akshay on 11/03/2018.
+ */
+module.exports = function (app) {
+
+    <!-- Models -->
+    var models = require('./model/models.server')();
+
+    <!-- Services -->
+    require('./services/user.service.server.js')(app, models.userModel);
+    require('./services/deduction.service.server.js')(app, models.deductionModel);
+};
