a84df33ee8a2b06fdb7dcf43378c0ed1fd0b47f0
Akshay Sadarangani <akshay.sadarangani@gmail.com>
2018-11-05 06:22:02 -0500

    Server revert

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var mongodb = require("mongodb");
 
 app.use(session({
     secret: 'this is the secret',    // process.env.SESSION_SECRET
@@ -19,28 +18,14 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-/*require ("./test/app.js")(app);*/
+require ("./test/app.js")(app);
 require("./server/app.js")(app);
 
-var db;
+//require("./passport/services/user.service.server");
+var port = process.env.PORT || 3000;
 
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    // Save database object from the callback for reuse.
-    db = client.db();
-    console.log("Database connection ready");
-    // Initialize the app.
-    /*require("./passport/services/user.service.server");*/
-    var port = process.env.PORT || 3000;
-
-    app.listen(port);
-});
+app.listen(port);
